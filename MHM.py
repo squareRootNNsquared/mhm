b0025e4aaab204ea4dee4c47226fa015a4c633d0
NN <nenunez@ucdavis.edu>
2017-07-31 11:38:43 -0700

    Update MHM.py

--- MHM.py
+++ MHM.py
@@ -29,10 +29,6 @@
 ###						List of codes:
 ###						M 	-	mass
 ###
-###
-###		Known Bug	[1]	Will perform most recent command (perhaps again) when program is started.
-###
-###		Warning	[1]	Reports close (>1s) in time proximity to a recent report may be ignored.
 ###		Warning [2] Data plotted with order corresponding to log.
 ###
 
@@ -97,6 +93,8 @@ def d(list_a,list_b,index,secToDay):
 	###
 	errFlag = 999.321
 	try :
+		numerator = errFlag
+		denominator = errFlag
 		if ((index-1) >= 0) :
 			numerator 	= (list_a[index]-list_a[index-1])
 			denominator	= (list_b[index]-list_b[index-1])
@@ -110,7 +108,7 @@ def d(list_a,list_b,index,secToDay):
 			d = errFlag
 	except IndexError :
 		d = errFlag
-	return d
+	return [d,numerator,denominator]
 
 def maxIndex(list):
 	### Returns largest list index and the last list index
@@ -214,157 +212,187 @@ def getFirstTextBlock(self, email_message_instance):
 ###
 while 1:
 
+	###
+	### Data Acquisition
+	###
+
 	###	Log into inbox of mailbox
 	MHM_mail = imaplib.IMAP4_SSL(par.em_imap)
 	MHM_mail.login(par.em_ac,par.em_pw)
-	MHM_mail.list()
-	MHM_mail.select("inbox")
+	MHM_mail.select('INBOX')
 
-	### Retrieve raw, most recent email
-	sResult, data	=	MHM_mail.uid('search',None,"ALL")
-	ID_latest		=	data[0].split(" ")[-1]
-	sResult, data	=	MHM_mail.uid('fetch',ID_latest,'(RFC822)')
-	latestEmail_raw	=	data[0][1]
+	### Retrieve unread email IDs
+	status, response = MHM_mail.search(None,'(UNSEEN)')
+	unreadList = response[0].split()
 
-	### Parse most recent, raw email
-	latestEmail			=	email.message_from_string(latestEmail_raw)
-	latestEmail_sender	=	email.utils.parseaddr(latestEmail['From'])[1]
-	latestEmail_body 	= 	getFirstTextBlock('',latestEmail)
-	latestEmail_time	=	t.mktime(email.utils.parsedate(latestEmail['Date']))
+	### If there is unaccessed data, continue to proccess
+	###		NOTE: [[ Each process, uncluding a NULL thereof, will perform a marking I/A ]]
+	if (len(unreadList) != 0) :
+		for ID_current in unreadList :
+			
+			### Fetch raw email
+			sResult, data	=	MHM_mail.uid('fetch',ID_current,'(RFC822)')
+			latestEmail_raw	=	data[0][1]
 
-	###	Abstract from latest email if not abstracted
-	if (latestEmail_time > emailTimeKeeper) :
-		emailTimeKeeper = latestEmail_time
-		data = latestEmail_body.split(" ")
-		for i in range(0,len(data)):
+			### Parse most recent, raw email
+			latestEmail			=	email.message_from_string(latestEmail_raw)
+			latestEmail_sender	=	email.utils.parseaddr(latestEmail['From'])[1]
+			latestEmail_body 	= 	getFirstTextBlock('',latestEmail)
+			latestEmail_time	=	t.mktime(email.utils.parsedate(latestEmail['Date']))
+			if (latestEmail_time > emailTimeKeeper) :
+				emailTimeKeeper = latestEmail_time
+				data = latestEmail_body.split(" ")
+				for i in range(0,len(data)):
 
-			###
-			###	MHM_M operation
-			###
+					###
+					###	MHM_M Operation
+					###
 
-			### If M value in retrieved data
-			if ("R~M" in str(data[i])) :
-				
-				###	Data Acquisition
-				data_M = eval(data[i].strip("R~M"))
-				data_M = float(data_M)
-				MHM_log_M	=	open("%s/MHM_log_M.txt"%(path()),"a+")
-				MHM_log_M.write("%f\t%f\t%f\n"%(data_M,latestEmail_time,t.time()))
-				MHM_log_M.close()
+					### If M value in retrieved data
+					if ("R~M" in str(data[i])) :
+						
+						###	Data Interpretation
+						data_M = eval(data[i].strip("R~M"))
+						data_M = float(data_M)
+						MHM_log_M	=	open("%s/MHM_log_M.txt"%(path()),"a+")
+						MHM_log_M.write("%f\t%f\t%f\n"%(data_M,latestEmail_time,t.time()))
+						MHM_log_M.close()
 
-				###
-				### Plotting and Processing Routine
-				###
+						### Flag data on server as seen 
+						MHM_mail.store(ID_current , '+FLAGS' , '\Seen')
 
-				### Define data
-				MHM_log_M		=	open("%s/MHM_log_M.txt"%(path()),"r")
-				M,time,logTime	=	np.loadtxt(MHM_log_M , delimiter="\t" , usecols=(0,1,2), unpack=True)
+						###
+						### Plotting and Processing Routine
+						###
 
-				### Format time into datetime ( : "dt" ) objects
-				time_dt  = []
-				if isinstance(time, np.float64):
-					item = time
-					time = []
-					time.append(item)
-				for j in range(0,len(time)) :
-					time_dt.append(dt.datetime.fromtimestamp(time[j]))
+						### Define data
+						MHM_log_M		=	open("%s/MHM_log_M.txt"%(path()),"r")
+						M,time,logTime	=	np.loadtxt(MHM_log_M , delimiter="\t" , usecols=(0,1,2), unpack=True)
 
-				### Form Derivative Data
-				max_ = maxIndex(time_dt)
-				dMAvgList = []
-				for o in range(0,len(time_dt)):		# For each data point -- UNOPTIMIZED -- Change: Remove earliest, append new
-					dMAvg = 0
-					dMCount = 0
-					for e in range(0,(o+1)):		# Calculate rolling average derivative at event "o" with prior events
-						if ( time_dt[e] >= (time_dt[o]-dtd(days=par.dM_dayRange)) ) :
-							dM = d(M,time,e,1)
-							if (dM != 999.321) :
-								dMAvg = dMAvg + dM
-								dMCount = dMCount +1
-					if (dMCount != 0) :
-						dMAvg = dMAvg/dMCount
-					if (dMCount == 0) :
-						dMAvg = 0
-					dMAvgList.append(dMAvg)
+						### Format time into datetime ( : "dt" ) objects
+						time_dt  = []
+						if isinstance(time, np.float64):
+							item = time
+							time = []
+							time.append(item)
+						for j in range(0,len(time)) :
+							time_dt.append(dt.datetime.fromtimestamp(time[j]))
 
-				### Rebuild expanded ( : "exp" ) log -- UNOPTIMIZED -- Change: Only update, not rebuild
-				open("%s/MHM_log_M_exp.txt"%(path()), 'w').close()
-				MHM_log_M_exp = open("%s/MHM_log_M_exp.txt"%(path()),"a+")
-				if isinstance(M, np.float64):
-					item = M
-					M = []
-					M.append(item)
-				if not (isinstance(logTime, list)) :
-					item = logTime
-					logTime = []
-					logTime.append(item)
-				if not (isinstance(logTime[0], float)) :
-					logTime = logTime[0]
-				for m in range(0,len(dMAvgList)):
-					if (math.isnan(dMAvgList[m])) :
-						dMAvgList[m] = 0.0
-				for k in range(0,len(M)):
-					MHM_log_M_exp.write("%f\t%f\t%f\t%f\t\n"%(M[k],time[k],dMAvgList[k],logTime[k]))
-				MHM_log_M_exp.close()
+						### Form Derivative Data
+						max_ = maxIndex(time_dt)
+						dMAvgList = []
+						for o in range(0,len(time_dt)):		# For each data point -- UNOPTIMIZED -- Change: Remove earliest, append new
+							dMAvg = 0
+							dTime = 0
+							for e in range(0,(o+1)):		# Calculate rolling average derivative at event "o" with prior events
+								if ( time_dt[e] >= (time_dt[o]-dtd(days=par.dM_dayRange)) ) :
+									dM = d(M,time,e,1)
+									if (dM[0] != 999.321) :
+										dMAvg = dMAvg + dM[0]
+										if (dM[2] != 999.321) :
+											dTime = dTime + dM[2]
+							if (dTime != 0) :
+								dMAvg = dMAvg/dTime
+							if (dTime == 0) :
+								dMAvg = 0
+							dMAvgList.append(dMAvg)
 
-				### Build plotting object
-				canvas,drawn = plt.subplots(figsize=(8,5))
-				plt.subplots_adjust(bottom=0.15)
+						### Rebuild expanded ( : "exp" ) log -- UNOPTIMIZED -- Change: Only update, not rebuild
+						open("%s/MHM_log_M_exp.txt"%(path()), 'w').close()
+						MHM_log_M_exp = open("%s/MHM_log_M_exp.txt"%(path()),"a+")
+						if isinstance(M, np.float64):
+							item = M
+							M = []
+							M.append(item)
+						if not (isinstance(logTime, list)) :
+							item = logTime
+							logTime = []
+							logTime.append(item)
+						if not (isinstance(logTime[0], float)) :
+							logTime = logTime[0]
+						for m in range(0,len(dMAvgList)):
+							if (math.isnan(dMAvgList[m])) :
+								dMAvgList[m] = 0.0
+						for k in range(0,len(M)):
+							MHM_log_M_exp.write("%f\t%f\t%f\t%f\t\n"%(M[k],time[k],dMAvgList[k],logTime[k]))
+						MHM_log_M_exp.close()
 
-				### Label plotting object
-				today = dt.datetime.now()
-				plt.title('Macroscopic Health Management')
-				plt.xlabel('Day of Year %s [day]'%(today.year))
-				
-				### Draw primary parameter of plotting object
-				drawn.tick_params('both' , length=7 , which='major')
-				drawn.plot(time_dt , M , 'b-')
-				drawn.set_ylabel('Weight [lbs]' , color='blue')
-				# [[ No ylim set ]]
+						### Build plotting object
+						canvas,drawn = plt.subplots(figsize=(8,5))
+						plt.subplots_adjust(bottom=0.15)
 
-				### Draw derivative parameter of plotting object
-				drawnDeriv = drawn.twinx()
-				drawnDeriv.tick_params('both' , length=7 , which='major')
-				drawnDeriv.plot(time_dt , dMAvgList , 'g-')
-				drawnDeriv.set_ylabel('%d Day Average of Change in Weight [lbs/day]'%(par.dM_dayRange) , color="green")
-				drawnDeriv.set_ylim(-3,3)
+						### Label plotting object
+						today = dt.datetime.now()
+						plt.title('Macroscopic Health Management')
+						plt.xlabel('Day of Year %s [day]'%(today.year))
+						
+						### Draw primary parameter of plotting object
+						drawn.tick_params('both' , length=7 , which='major')
+						drawn.plot(time_dt , M , 'b-')
+						drawn.set_ylabel('Weight [lbs]' , color='blue')
+						# [[ No ylim set ]]
 
-				### Format horizontal axis of plotting object
-				plt.setp(drawn.get_xticklabels() , rotation='35')
-				daily = dates.DayLocator()
-				hourly = aml()
-				dailyFormat = dates.DateFormatter('%b%d')
-				drawn.xaxis.set_major_locator(daily)
-				drawn.xaxis.set_major_formatter(dailyFormat)
-				drawn.xaxis.set_minor_locator(hourly)
-				prior = today - dtd(days=par.plotTimeRange)
-				timeMax = dt.date(today.year , today.month , today.day+1)
-				timeMin = dt.date(prior.year , prior.month , prior.day)
-				drawn.set_xlim(timeMin,timeMax)
+						### Draw derivative parameter of plotting object
+						drawnDeriv = drawn.twinx()
+						drawnDeriv.tick_params('both' , length=7 , which='major')
+						drawnDeriv.plot(time_dt , dMAvgList , 'g-')
+						drawnDeriv.set_ylabel('%d Day Time-Average of Change [lbs/day]'%(par.dM_dayRange) , color="green")
+						drawnDeriv.set_ylim(-3,3)
 
-				### Draw primary parameter limit within plotting object
-				drawn.axhline(y=par.M_goal , linewidth=10 , color='#00ff00', alpha=0.5)
+						### Format horizontal axis of plotting object
+						tomorrow = dt.datetime.fromtimestamp(t.time()+(24*60*60))
+						plt.setp(drawn.get_xticklabels() , rotation='35')
+						daily = dates.DayLocator()
+						hourly = aml()
+						dailyFormat = dates.DateFormatter('%b%d')
+						drawn.xaxis.set_major_locator(daily)
+						drawn.xaxis.set_major_formatter(dailyFormat)
+						drawn.xaxis.set_minor_locator(hourly)
+						prior = today - dtd(days=par.plotTimeRange)
+						timeMax = dt.date(tomorrow.year , tomorrow.month , tomorrow.day)
+						timeMin = dt.date(prior.year , prior.month , prior.day)
+						drawn.set_xlim(timeMin,timeMax)
 
-				### Draw [ dM/dt = 0 ] line
-				drawnDeriv.axhline(y=0 , linewidth=1 , color='#000000', alpha=0.5)				
+						### Draw primary parameter limit within plotting object
+						drawn.axhline(y=par.M_goal , linewidth=10 , color='#00ff00', alpha=0.25)
 
-				### Save drawn image and clear drawing frame
-				plt.savefig('%s/MHM_plot_M.png'%(path()))
-				plt.clf()
-				plt.cla()
+						### Draw [ dM/dt = 0 ] line
+						drawnDeriv.axhline(y=0 , linewidth=1 , color='#000000', alpha=0.25)				
 
-			### If data request in retrieved data, send plot ###
-			if ("RD~M" in str(data[i])):
-				sendEmail("MHM Plot Delivery","M[T]",par.dataRecipient,"%s/MHM_plot_M.png"%(path()))
+						### Save drawn image and clear drawing frame
+						plt.savefig('%s/MHM_plot_M.png'%(path()))
+						plt.clf()
+						plt.cla()
+
+					###
+					###	MHM_DataDelivery Operation
+					###
+
+					### If data request in retrieved data, send plot ###
+					if ("RD~M" in str(data[i])):
+
+						### Delivery data
+						sendEmail("MHM Plot Delivery","M[T]",par.dataRecipient,"%s/MHM_plot_M.png"%(path()))
+
+						### Flag data on server as seen 
+						MHM_mail.store(ID_current , '+FLAGS' , '\Seen')
+
+					###
+					###	MHM_NULL Operation
+					###
+					
+					### Flag data on server as seen 
+					MHM_mail.store(ID_current , '+FLAGS' , '\Seen')
 
 	### End Of Routine operations
-	t.sleep(5)
+	t.sleep(10)
 	print "[ MHM Operating ][ Uptime = %fminutes ]"%((t.time()-initialTimeStamp)/(60))
 	par = reload(par)
 
 ###
 ###	Features to add:
 ###
+###		-	emailTimeKeeper needs to be non-volatized
 ###		-	Develop and implement Smoothing and Interpolation function
 ###		-	Goal line tolerance
 ###		-	Add [Software Redundancy]
